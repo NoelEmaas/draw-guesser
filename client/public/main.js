d72cc7af199af0362812b9fb2186892f271ce5c7
Noel Emaas <emaasnoel01@gmail.com>
2024-05-11 21:03:40 +0800

    feat: added initial files

---
+++ client/public/main.js
@@ -0,0 +1,25 @@
+const electron = require("electron");
+const path = require("path");
+
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    autoHideMenuBar: true,
+    webPreferences: {
+        nodeIntegration: true,
+        contextIsolation: false,
+        enableRemoteModule: true,
+    },
+  });
+
+  console.log(__dirname);
+  mainWindow.loadFile(path.join(__dirname, "../build/index.html"));
+}
+
+app.on("ready", createWindow);
---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 class="text-red-500 font-bold">Hello</h1>
+    </div>
+  );
+}
+
+export default App;
